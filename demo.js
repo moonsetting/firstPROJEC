248978df95e72212b68331f1bd1a46144dfc6f9a
moonsetting <121739985+moonsetting@users.noreply.github.com>
2021-05-29 22:02:26 +0000

    Release 0.13.2

--- demo.js
+++ demo.js
@@ -4,4 +4,4 @@
 // function that represents a service factory, a show callback that displays the
 // final result, and a parseq routine written as an annotated nested array.
 
-// This interacts with the
+// This interacts with the browser using Plain
