53fd909cf5dba4bfc61ba6ff8406f47c09002865
moonsetting <121739985+moonsetting@users.noreply.github.com>
2021-07-19 19:29:56 +0000

    changes to README

--- demo.js
+++ demo.js
@@ -23,4 +23,4 @@ document.getElementById("reset").onclick = function (ignore) {
 };
 
 function widget(name) {
-    return function widget_requestor(callbac
+    return function widget_requestor(callback, value) 
