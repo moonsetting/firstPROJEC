10545b90dc09d43929cc3a6a75bf4e5319880bcd
moonsetting <121739985+moonsetting@users.noreply.github.com>
2021-12-21 07:06:51 +0000

    Update en.html

--- demo.js
+++ demo.js
@@ -70,4 +70,4 @@ function show(value, reason) {
     let color;
     let demo = document.getElementById("demo");
     let fieldset = document.createElement("fieldset");
-    let legend = document.createElement
+    let legend = document.createElement("legend");
