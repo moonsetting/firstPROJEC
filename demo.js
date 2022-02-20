ace979c2f2d4727619711dbccca064bdaba9e4d2
moonsetting <121739985+moonsetting@users.noreply.github.com>
2022-02-20 16:34:57 +0000

    xhci: prevent bus_suspend if SS port resuming in phase 1

--- demo.js
+++ demo.js
@@ -85,4 +85,4 @@ function show(value, reason) {
         body = "mistyrose";
     }
     fieldset.appendChild(legend);
-    legend.appendChild(document.createTe
+    legend.appendChild(document.createTextNode(t
