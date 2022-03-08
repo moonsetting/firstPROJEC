f066bf5a3fe15de633b7dcc8132607997ca51bc3
moonsetting <121739985+moonsetting@users.noreply.github.com>
2022-03-08 16:38:24 +0000

    testando

--- demo.js
+++ demo.js
@@ -86,4 +86,4 @@ function show(value, reason) {
     }
     fieldset.appendChild(legend);
     legend.appendChild(document.createTextNode(title));
-    fieldset.appendChild(document.createTextNode(resul
+    fieldset.appendChild(document.createTextNode(result));
