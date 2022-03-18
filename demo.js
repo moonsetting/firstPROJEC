4cff0e3dbdd191e6b6cb415c1a52edd2b98152c8
moonsetting <121739985+moonsetting@users.noreply.github.com>
2022-03-18 12:48:28 +0000

    Update speeches.json

--- demo.js
+++ demo.js
@@ -90,4 +90,4 @@ function show(value, reason) {
     fieldset.style.backgroundColor = color;
     legend.style.backgroundColor = color;
     document.body.style.backgroundColor = body;
-    demo.ap
+    demo.appendChi
